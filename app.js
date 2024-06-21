81eeace741cdfd62dcf19b42af162ad2efda4281
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-06-21 07:58:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+/* Created by Tivotal */
+
+let typingText = document.querySelector(".typing-text");
+
+let typeText = () => {
+  setTimeout(() => {
+    typingText.textContent = "Web Developer";
+  }, 0);
+  setTimeout(() => {
+    typingText.textContent = "App Developer";
+  }, 4000);
+  setTimeout(() => {
+    typingText.textContent = "IOS Developer";
+  }, 8000);
+};
+
+typeText();
+setInterval(typeText, 12000);
